9918b371b7df7fe6a23802268eaf14570b6b7b24
Christ Eliezier Bouboue <ton_email_github@example.com>
2025-11-02 20:35:20 +0000

    Deploy: version production — overwrite remote

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-
-/**
- * Redirection vers le dossier public/
- * Ce fichier permet d'accéder au site si le Document Root
- * n'est pas configuré vers le dossier public/
- */
-
 header('Location: public/index.php');
 exit;
-
